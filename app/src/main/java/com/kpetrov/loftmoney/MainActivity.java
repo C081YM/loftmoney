10610d5c0b6249bd6bbbbb1d5f8dda8a3d732d30
Kirill Petrov <kirill527@rambler.ru>
2020-07-24 18:53:45 +0500

    Добавил ViewPager

--- app/src/main/java/com/kpetrov/loftmoney/MainActivity.java
+++ app/src/main/java/com/kpetrov/loftmoney/MainActivity.java
@@ -1,7 +1,12 @@
 package com.kpetrov.loftmoney;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
@@ -13,16 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.fragment_container, new BudgetFragment());
-        transaction.commit();
+        TabLayout tabLayout = findViewById(R.id.tabs);
 
-            TabLayout tabLayout = findViewById(R.id.tabs);
-            tabLayout.addTab(tabLayout.newTab().setText(R.string.expenses));
-            tabLayout.addTab(tabLayout.newTab().setText(R.string.income));
-            tabLayout.addTab(tabLayout.newTab().setText(R.string.balance));
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new BudgetPagerAdapter(getSupportFragmentManager(),FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
 
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setText(R.string.expenses);
+        tabLayout.getTabAt(1).setText(R.string.income);
+        tabLayout.getTabAt(2).setText(R.string.balance);
     }
+
+    static class BudgetPagerAdapter extends FragmentPagerAdapter {
+
+        public BudgetPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new BudgetFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
 }
 
 
